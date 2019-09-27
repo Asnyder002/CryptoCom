a3a2185f72982872b3b8d51d26a39679cc6eda4d
Adam Snyder <adam296@live.com>
2019-09-26 23:56:55 -0500

    Created mock CryptoComServer

---
+++ src/ServerSide/CryptoComServer.java
@@ -0,0 +1,31 @@
+package ServerSide;
+
+import java.rmi.*;
+import RemoteObject.*;
+
+public class CryptoComServer {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("CryptoComServer starting...");
+        System.setSecurityManager(new SecurityManager());
+        System.out.println("Security Manager installed, Setting up service");
+        
+        try {
+            CryptoComManagerImpl ccm = new CryptoComManagerImpl();
+            
+            System.out.println("Publishing CryptoComServer...");
+            
+            Naming.rebind(CryptoComManager.LOOKUPNAME, ccm);
+            
+            System.out.println("CryptoComServer ready.");
+        }
+        catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+            
+        }
+        
+    }
+    
+}
