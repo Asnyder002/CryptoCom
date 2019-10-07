fa39f7b1529641d2064d604f4fec96892a72c3a6
Adam Snyder <adam296@live.com>
2019-10-07 00:25:16 -0500

    Added comments to CCM class

--- CryptoComClient/src/cryptocomclient/CryptoComClient.java
+++ CryptoComClient/src/cryptocomclient/CryptoComClient.java
@@ -9,34 +9,41 @@ public class CryptoComClient {
     String userName;
     CryptoComManager ccm;
     
+    // Constructor that takes in a reference to the remote object.
     public CryptoComClient(CryptoComManager ccm) {
         this.ccm = ccm;
     }
     
+    // Creates a new instance of Message and returns it.
     public Message createNewMessage(String memo, String sender, String recipient) {
         
         return new Message(memo, sender, recipient);
         
     }
     
+    // Returns a message from the recived arrayList at the specified index.
     public Message openMessage(int num) {
         
         return receivedMessageList.get(num);
         
     }
     
+    // Adds a message to the received message arrayList
     public void addReceivedMessage(Message message) {
         
         receivedMessageList.add(message);
         
     }
     
-    public void removeRecivedMessage(Message message) {
+    // Removes a message from the received message arrayList
+    public void removeReceivedMessage(Message message) {
         
         receivedMessageList.remove(message);
         
     }
     
+    
+    // Getters and setters for fields listed at the top
     public ArrayList<Message> getRecievedMessageList() {return receivedMessageList;}
     
     public String getUserName() {return userName;}
