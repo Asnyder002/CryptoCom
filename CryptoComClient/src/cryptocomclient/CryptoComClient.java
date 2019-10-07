16d89bdd152fc6e3084f51f17f6693c997b4c3af
Adam Snyder <adam296@live.com>
2019-10-06 23:32:18 -0500

    Created CryptoComClient method templates. Ready to be filled in.

--- CryptoComClient/src/cryptocomclient/CryptoComClient.java
+++ CryptoComClient/src/cryptocomclient/CryptoComClient.java
@@ -1,7 +1,40 @@
 package cryptocomclient;
 
+import RemoteObject.CryptoComManager;
 import java.util.ArrayList;
 
 public class CryptoComClient {
+    
+    ArrayList<Message> receivedMessageList = new ArrayList<>();
+    String userName;
+    CryptoComManager ccm;
+    
+    public CryptoComClient(CryptoComManager ccm) {
+        this.ccm = ccm;
+    }
+    
+    public Message createNewMessage(String memo, String sender, String recipient) {
+        
+    }
+    
+    public Message openMessage(int num) {
+        
+    }
+    
+    public void addReceivedMessage(Message message) {
+        
+    }
+    
+    public void removeRecivedMessage() {
+        
+    }
+    
+    public ArrayList<Message> getRecievedMessageList() {return receivedMessageList;}
+    
+    public String getUserName() {return userName;}
+    public void setUserName(String userName) {this.userName = userName;}
+    
+    public CryptoComManager getCCM() {return ccm;}
+    public void setCCM(CryptoComManager ccm) {this.ccm = ccm;}
     
 }
