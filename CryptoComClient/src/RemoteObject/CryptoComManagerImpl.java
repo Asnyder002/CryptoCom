8b154a5be07b9e33b6b85875cee0c580fdb63104
Adam Snyder <adam296@live.com>
2019-10-09 11:52:06 -0500

    CCMImpl: Created sendMessageToServer method

--- CryptoComClient/src/RemoteObject/CryptoComManagerImpl.java
+++ CryptoComClient/src/RemoteObject/CryptoComManagerImpl.java
@@ -1,26 +1,39 @@
 package RemoteObject;
 
+import cryptocomclient.Message;
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CryptoComManagerImpl extends UnicastRemoteObject implements CryptoComManager{
 
-    ArrayList<String> stringList = new ArrayList<>();
+    HashMap<String, ArrayList<Message>> messageMap = new HashMap<>();
     
     public CryptoComManagerImpl() throws RemoteException{
         super();
     }
-    
+
     @Override
-    public void sendString(String message) throws RemoteException {
-        stringList.add(message);
+    public void sendMessageToServer(Message message) throws RemoteException {
+        
+        // Checks to see if the recipient already has an arraylist
+        // If it doesn't creates a new arraylist with the recipient as the key
+        if(!messageMap.containsKey(message.getRecipient())){
+            messageMap.put(message.getRecipient(), new ArrayList<Message>());
+        }
+        
+        // Gets the recipients Arraylist and adds the new message
+        ArrayList<Message> recipientArrayList = messageMap.get(message.getRecipient());
+        recipientArrayList.add(message);
     }
 
     @Override
-    public String receiveString() throws RemoteException {
-        return stringList.remove(0);
+    public ArrayList<Message> receiveMessagesFromServer() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+
     
     
 }
