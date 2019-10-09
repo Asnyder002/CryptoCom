c7b9c6f3a1292c2a768736a1dc5735c4525a5736
Adam Snyder <adam296@live.com>
2019-10-09 11:22:43 -0500

    Changed arraylist to be the return type in recieveMessage method

--- CryptoComClient/src/RemoteObject/CryptoComManager.java
+++ CryptoComClient/src/RemoteObject/CryptoComManager.java
@@ -2,10 +2,11 @@ package RemoteObject;
 
 import cryptocomclient.Message;
 import java.rmi.*;
+import java.util.ArrayList;
 
 public interface CryptoComManager extends Remote{
     
     public final static String LOOKUPNAME = "CryptoComManager";
     public void sendMessageToServer(Message message) throws RemoteException;
-    public Message receiveMessagesFromServer() throws RemoteException;
+    public ArrayList<Message> receiveMessagesFromServer() throws RemoteException;
 }
