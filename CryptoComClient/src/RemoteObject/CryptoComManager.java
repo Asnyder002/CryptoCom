73a48538838250353169ffbb659f4c83b9e35115
Adam Snyder <adam296@live.com>
2019-10-09 11:57:44 -0500

    Changed CCM interface to pass a string to call receiveMessagesFromServer method in impl

--- CryptoComClient/src/RemoteObject/CryptoComManager.java
+++ CryptoComClient/src/RemoteObject/CryptoComManager.java
@@ -8,5 +8,5 @@ public interface CryptoComManager extends Remote{
     
     public final static String LOOKUPNAME = "CryptoComManager";
     public void sendMessageToServer(Message message) throws RemoteException;
-    public ArrayList<Message> receiveMessagesFromServer() throws RemoteException;
+    public ArrayList<Message> receiveMessagesFromServer(String userName) throws RemoteException;
 }
