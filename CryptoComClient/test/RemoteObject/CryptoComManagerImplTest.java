83e30f65c0abe66462e36b46cd49db6337351a3b
Adam Snyder <adam296@live.com>
2019-10-12 17:31:59 -0500

    Added comments to test method

--- CryptoComClient/test/RemoteObject/CryptoComManagerImplTest.java
+++ CryptoComClient/test/RemoteObject/CryptoComManagerImplTest.java
@@ -17,15 +17,19 @@ public class CryptoComManagerImplTest {
     @Test
     public void testSendMessageToServer() throws Exception {
         
+        // Creates ccmImpl accesed by interface
+        // New message created and passed to the ccm method
         CryptoComManager ccm = new CryptoComManagerImpl();
         Message message = new Message("Testing", "Adam", "Bob");
         ccm.sendMessageToServer(message);
         
-        Message expected = message;
-        
+        // cast from interface to impl object for gethashmap use
+        // get hashmap, get arraylist, get message
         CryptoComManagerImpl ccmImpl = (CryptoComManagerImpl)ccm;
         HashMap<String, ArrayList<Message>> hashMap = ccmImpl.getHashMap();
         ArrayList<Message> messageList = hashMap.get("Bob");
+        
+        Message expected = message;
         Message actual = messageList.get(0);
         
         assertEquals(expected, actual);
@@ -36,6 +40,9 @@ public class CryptoComManagerImplTest {
      */
     @Test
     public void testReceiveMessagesFromServer() throws Exception {
+        
+        
+        
     }
     
 }
