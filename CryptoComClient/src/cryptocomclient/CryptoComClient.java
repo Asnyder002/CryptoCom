95b1dd21d1ad0953baedb0923c54e214686baaea
Adam Snyder <adam296@live.com>
2019-10-12 16:47:25 -0500

    Changed the way you receive message from the remote object in the client method GetReceivedMessage

--- CryptoComClient/src/cryptocomclient/CryptoComClient.java
+++ CryptoComClient/src/cryptocomclient/CryptoComClient.java
@@ -1,6 +1,7 @@
 package cryptocomclient;
 
 import RemoteObject.CryptoComManager;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public class CryptoComClient {
@@ -16,6 +17,8 @@ public class CryptoComClient {
     
     // Creates a new instance of Message and returns it.
     public Message createNewMessage(String memo, String sender, String recipient) {
+        //Encryption should be done here
+        
         
         return new Message(memo, sender, recipient);
         
@@ -28,13 +31,20 @@ public class CryptoComClient {
         
     }
     
-    // Adds a message to the received message arrayList
-    public void addReceivedMessage(ArrayList<Message> receivedList) {
+    // Calls the remote object to get the stored messages then adds them to the
+    // clients receivedMessageList
+    public void getReceivedMessage() throws RemoteException {
+        
+        ArrayList<Message> receivedList = ccm.receiveMessagesFromServer(userName);
         
         for(Message message: receivedList) {
             receivedMessageList.add(message);
         }
       
+    }
+    
+    public void sendMessage(Message message) throws RemoteException {
+        ccm.sendMessageToServer(message);
     }
     
     // Removes a message from the received message arrayList
