256a2cb875ee64ea2401c597fb6917381f069d55
Adam Snyder <adam296@live.com>
2019-10-12 17:29:21 -0500

    Created Test for CryptoComManagerImpl. testSendMessageToServer passed

---
+++ CryptoComClient/test/RemoteObject/CryptoComManagerImplTest.java
@@ -0,0 +1,41 @@
+package RemoteObject;
+
+import cryptocomclient.Message;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CryptoComManagerImplTest {
+    
+    public CryptoComManagerImplTest() {
+    }
+
+    /**
+     * Test of sendMessageToServer method, of class CryptoComManagerImpl.
+     */
+    @Test
+    public void testSendMessageToServer() throws Exception {
+        
+        CryptoComManager ccm = new CryptoComManagerImpl();
+        Message message = new Message("Testing", "Adam", "Bob");
+        ccm.sendMessageToServer(message);
+        
+        Message expected = message;
+        
+        CryptoComManagerImpl ccmImpl = (CryptoComManagerImpl)ccm;
+        HashMap<String, ArrayList<Message>> hashMap = ccmImpl.getHashMap();
+        ArrayList<Message> messageList = hashMap.get("Bob");
+        Message actual = messageList.get(0);
+        
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test of receiveMessagesFromServer method, of class CryptoComManagerImpl.
+     */
+    @Test
+    public void testReceiveMessagesFromServer() throws Exception {
+    }
+    
+}
