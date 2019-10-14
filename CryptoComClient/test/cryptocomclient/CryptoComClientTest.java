408a25b098fc28b422d40e475db7601922a59a1b
Adam Snyder <adam296@live.com>
2019-10-13 21:01:40 -0500

    Created client unit test class

---
+++ CryptoComClient/test/cryptocomclient/CryptoComClientTest.java
@@ -0,0 +1,84 @@
+package cryptocomclient;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CryptoComClientTest {
+    
+    public CryptoComClientTest() {
+    }
+
+    /**
+     * Test of createNewMessage method, of class CryptoComClient.
+     */
+    @Test
+    public void testCreateNewMessage() {
+        
+        
+        
+    }
+
+    /**
+     * Test of openMessage method, of class CryptoComClient.
+     */
+    @Test
+    public void testOpenMessage() {
+    }
+
+    /**
+     * Test of sendMessage method, of class CryptoComClient.
+     */
+    @Test
+    public void testSendMessage() throws Exception {
+    }
+
+    /**
+     * Test of getReceivedMessages method, of class CryptoComClient.
+     */
+    @Test
+    public void testGetReceivedMessages() throws Exception {
+    }
+
+    /**
+     * Test of removeReceivedMessage method, of class CryptoComClient.
+     */
+    @Test
+    public void testRemoveReceivedMessage() {
+    }
+
+    /**
+     * Test of getRecievedMessageList method, of class CryptoComClient.
+     */
+    @Test
+    public void testGetRecievedMessageList() {
+    }
+
+    /**
+     * Test of getUserName method, of class CryptoComClient.
+     */
+    @Test
+    public void testGetUserName() {
+    }
+
+    /**
+     * Test of setUserName method, of class CryptoComClient.
+     */
+    @Test
+    public void testSetUserName() {
+    }
+
+    /**
+     * Test of getCCM method, of class CryptoComClient.
+     */
+    @Test
+    public void testGetCCM() {
+    }
+
+    /**
+     * Test of setCCM method, of class CryptoComClient.
+     */
+    @Test
+    public void testSetCCM() {
+    }
+    
+}
