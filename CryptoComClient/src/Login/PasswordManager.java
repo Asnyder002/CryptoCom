f25ae3123fb84f8c9ce01a4f59f692076f456f2f
Adam Snyder <adam296@live.com>
2019-10-28 21:55:57 -0500

    Created outline and a few methods for a PasswordManager class

---
+++ CryptoComClient/src/Login/PasswordManager.java
@@ -0,0 +1,36 @@
+package Login;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class PasswordManager {
+    
+    private final static int iteration = 65536;
+    private final static int keyLength = 128;
+    private final static String algorithm = "PBKDF2WithHmacSHA1";
+    
+    public static byte[] generateSalt(){
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+    
+    public static byte[] generateHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException{
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iteration, keyLength);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(algorithm);
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+        return hash;
+    }
+    
+    public static boolean compareHash(byte[] storedPass, byte[] enteredPass) {
+        // compare that both hash are the same
+        //if true return true
+        return true;
+    }
+    
+}
