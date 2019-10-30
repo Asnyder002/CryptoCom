3929616e4f7a74fe7808f5939e35435cbc2620a9
Adam Snyder <adam296@live.com>
2019-10-30 11:40:17 -0500

    Implemented compareHash method

--- CryptoComClient/src/Login/PasswordManager.java
+++ CryptoComClient/src/Login/PasswordManager.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.Arrays;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
@@ -28,9 +29,7 @@ public class PasswordManager {
     }
     
     public static boolean compareHash(byte[] storedPass, byte[] enteredPass) {
-        // compare that both hash are the same
-        //if true return true
-        return true;
+        return Arrays.equals(storedPass, enteredPass);
     }
     
 }
