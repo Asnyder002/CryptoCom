58f62605276cf2e67b1e7f45d83a99660199b59b
Adam Snyder <adam296@live.com>
2019-10-30 12:15:23 -0500

    Implemented validateCredentials method

--- CryptoComClient/src/Login/LoginModel.java
+++ CryptoComClient/src/Login/LoginModel.java
@@ -15,13 +15,20 @@ public class LoginModel {
         this.ccm = ccm;
     }
     
-    public boolean validateCredentials(){
+    public boolean validateCredentials() throws NoSuchAlgorithmException, InvalidKeySpecException{
         // Check username to see if it's registered on the remote object
-        // If it is, pull the salt and has from the remote object
-        // Take the entered password and hash it with the salt
-        // Compare both hashes, if they are the same allow access.
-        // Otherwise inform that an in correct password was given.
-        return true;
+        if(ccm.usernameTaken(username)) {
+            // If it is, pull the salt and has from the remote object
+            User user = ccm.getUser(username);
+            byte[] salt = user.getSalt();
+            // Take the entered password and hash it with the salt
+            byte[] enteredPasswordHashed = PasswordManager.generateHash(password, salt);
+            // Compare both hashes, if they are the same allow access.
+            return PasswordManager.compareHash(user.getHashedPassword(), enteredPasswordHashed);
+            // Otherwise inform that an in correct password was given.
+        }
+
+        return false;
     }
     
     public boolean createNewUser() throws NoSuchAlgorithmException, InvalidKeySpecException {
@@ -34,9 +41,9 @@ public class LoginModel {
             User user = new User(username, hashedPassword, salt);
             // Store the username, salt, and hashed password on the remote object
             ccm.addNewUser(user);
+            return true;
         }
-        
-        
+        return false;
     }
     
     public String getUsername() {
