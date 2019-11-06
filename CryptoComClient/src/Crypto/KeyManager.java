777d752f1290c57fea2c940265de28fa4f4b7dcc
Adam Snyder <adam296@live.com>
2019-11-06 00:13:44 -0600

    Created a method to get public key from file

--- CryptoComClient/src/Crypto/KeyManager.java
+++ CryptoComClient/src/Crypto/KeyManager.java
@@ -8,6 +8,12 @@ package Crypto;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 /**
  *
@@ -15,7 +21,7 @@ import java.io.IOException;
  */
 public class KeyManager {
     
-    public void saveKeyToFile(String path, byte[] key) throws IOException {
+    public static void saveKeyToFile(String path, byte[] key) throws IOException {
         File file = new File(path);
         file.getParentFile().mkdirs();
         
@@ -23,6 +29,24 @@ public class KeyManager {
         outputStream.write(key);
         outputStream.flush();
         outputStream.close();
-}
+    }
+    
+    public static PublicKey getPublicKeyFromFile(String base64PublicKey) {
+        PublicKey publicKey = null;
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch(InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
+    
     
 }
