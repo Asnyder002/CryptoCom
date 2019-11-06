85aa848b4e007aed7d5c8bf370bc0d8ca977b234
Adam Snyder <adam296@live.com>
2019-11-05 23:40:41 -0600

    Created constuctor that generators and saves to private variables the keys

--- CryptoComClient/src/Crypto/KeyGenerator.java
+++ CryptoComClient/src/Crypto/KeyGenerator.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Crypto;
 
-/**
- *
- * @author Adam
- */
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 public class KeyGenerator {
+    
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+    
+    public KeyGenerator() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024);
+        KeyPair keyPair = keyGen.generateKeyPair();
+        this.publicKey = keyPair.getPublic();
+        this.privateKey = keyPair.getPrivate();
+    }
     
 }
