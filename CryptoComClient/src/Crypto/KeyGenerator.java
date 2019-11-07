a0b7ed02e7b9739b59a2e34c8afcef663f2a89e2
Adam Snyder <adam296@live.com>
2019-11-07 09:37:09 -0600

    Reafactored key related methods into one class

--- CryptoComClient/src/Crypto/KeyGenerator.java
+++ CryptoComClient/src/Crypto/KeyGenerator.java
@@ -1,10 +1,17 @@
 package Crypto;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class KeyGenerator {
     
@@ -17,6 +24,37 @@ public class KeyGenerator {
         KeyPair keyPair = keyGen.generateKeyPair();
         this.publicKey = keyPair.getPublic();
         this.privateKey = keyPair.getPrivate();
+    }
+    
+    public void saveKeyToFile(String path, byte[] key) throws IOException {
+        File file = new File(path);
+        file.getParentFile().mkdirs();
+        
+        FileOutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(key);
+        outputStream.flush();
+        outputStream.close();
+    }
+    
+    public static PublicKey getPublicKeyFromFile(String base64PublicKey) {
+        
+        // Needs to take in a file
+        // Open and pull base64Public key from file
+        
+        PublicKey publicKey = null;
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch(InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
     }
     
     public PublicKey getPublicKey() {
