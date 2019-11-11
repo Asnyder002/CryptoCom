dda173f87e9b6c49582f4a1f963ace6cdfbd4d56
Adam Snyder <adam296@live.com>
2019-11-10 19:36:36 -0600

    Created AES crypto class

---
+++ CryptoComClient/src/Crypto/CryptoAES.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+
+/**
+ *
+ * @author Adam
+ */
+public class CryptoAES {
+    
+    public static byte[] encrypt(String data, SecretKey secretKey){ 
+        
+    }
+    
+}
