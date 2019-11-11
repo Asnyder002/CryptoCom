d10f54142ca9ac044f2b19c28a5e17670b87ed69
Adam Snyder <adam296@live.com>
2019-11-10 20:29:12 -0600

    changed public/private key to the correct secret key

--- CryptoComClient/src/Crypto/CryptoAES.java
+++ CryptoComClient/src/Crypto/CryptoAES.java
@@ -21,15 +21,15 @@ import javax.crypto.SecretKey;
  */
 public class CryptoAES {
     
-    public static byte[] encrypt(String data, PublicKey publicKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{ 
+    public static byte[] encrypt(String data, SecretKey secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{ 
         Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         return cipher.doFinal(data.getBytes());
     }
     
-    public static String decrypt(byte[] data, PrivateKey privateKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    public static String decrypt(byte[] data, SecretKey secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
         return new String(cipher.doFinal(data));
     }
     
