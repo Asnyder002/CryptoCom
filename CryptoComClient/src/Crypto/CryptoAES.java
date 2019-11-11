b4888b6be3d2e4380e45ed10e991a8b87d180dcd
Adam Snyder <adam296@live.com>
2019-11-10 19:42:25 -0600

    added decrypt method to AES

--- CryptoComClient/src/Crypto/CryptoAES.java
+++ CryptoComClient/src/Crypto/CryptoAES.java
@@ -5,7 +5,12 @@
  */
 package Crypto;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
 /**
@@ -14,8 +19,16 @@ import javax.crypto.SecretKey;
  */
 public class CryptoAES {
     
-    public static byte[] encrypt(String data, SecretKey secretKey){ 
-        
+    public static byte[] encrypt(String data, SecretKey secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{ 
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return cipher.doFinal(data.getBytes());
+    }
+    
+    public static String decrypt(byte[] data, SecretKey secretKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return new String(cipher.doFinal(data));
     }
     
 }
