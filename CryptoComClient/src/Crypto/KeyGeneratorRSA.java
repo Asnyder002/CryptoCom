8a7adf328ae7825cb07aba9cac3c3fa0e56c8373
Adam Snyder <adam296@live.com>
2019-11-10 19:45:20 -0600

    Changed 1024 bit to 2048 bit RSA encryption

--- CryptoComClient/src/Crypto/KeyGeneratorRSA.java
+++ CryptoComClient/src/Crypto/KeyGeneratorRSA.java
@@ -14,7 +14,7 @@ public class KeyGeneratorRSA {
     // Creates RSA 1024 encrypted keys and saves them to the private fields
     public KeyGeneratorRSA() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(1024);
+        keyGen.initialize(2048);
         KeyPair keyPair = keyGen.generateKeyPair();
         this.publicKey = keyPair.getPublic();
         this.privateKey = keyPair.getPrivate();
